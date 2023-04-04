811da2a62ad03edb0a245062a2b7ccf4e74710c0
C0123G1-NguyenDucTrung <124018669+DucTrungC0123G1@users.noreply.github.com>
2023-04-05 00:49:54 +0700

    update

---
+++ ss2_Array_Loop/bai_tap/SumArrayColum.java
@@ -0,0 +1,29 @@
+package ss2_Array_Loop.bai_tap;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SumArrayColum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap so hang");
+        int n = Integer.parseInt(sc.nextLine());
+        System.out.println("Nhap so cot");
+        int m = Integer.parseInt(sc.nextLine());
+        int[][] arr = new int[n][m];
+        System.out.println("Nhap phan tu trong mang");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(sc.nextLine());
+            }
+
+        }
+        System.out.println("Nhap cot can tinh");
+        int col = Integer.parseInt(sc.nextLine());
+        int sum = 0;
+        for (int[] ints : arr) {
+            sum += ints[col];
+        }
+        System.out.println("Tong cot thu " + col + " la " + sum);
+    }
+}
