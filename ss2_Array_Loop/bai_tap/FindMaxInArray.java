4ced57b5e783f4f5931edbdf50dfbe9e72a29e58
C0123G1-NguyenDucTrung <124018669+DucTrungC0123G1@users.noreply.github.com>
2023-04-04 22:08:57 +0700

    update

--- ss2_Array_Loop/bai_tap/FindMaxInArray.java
+++ ss2_Array_Loop/bai_tap/FindMaxInArray.java
@@ -1,14 +1,30 @@
 package ss2_Array_Loop.bai_tap;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class FindMaxInArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Nhap do dai mang 1");
+        System.out.println("Nhap do dai hang");
         int n = Integer.parseInt(sc.nextLine());
-        System.out.println("Nhap do dai mang 2");
+        System.out.println("Nhap do dai cot");
         int m = Integer.parseInt(sc.nextLine());
-        int [][] arr = new int[n][m];
+        int[][] arr = new int[n][m];
+        int max = 0;
+        System.out.println("Nhap phan tu cua mang");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr[i].length; j++) {
+                if (max < arr[i][j]) {
+                    max = arr[i][j];
+                }
+            }
+        }
+        System.out.println("So lon nhat trong mang la :" + max);
     }
 }
---
+++ ss2_Array_Loop/bai_tap/FindMinInArray.java
@@ -0,0 +1,10 @@
+package ss2_Array_Loop.bai_tap;
+
+import java.util.Scanner;
+
+public class FindMinInArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
