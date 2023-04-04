70c9d60f74570a1aa4c899a00c7143914dbdb239
C0123G1-NguyenDucTrung <124018669+DucTrungC0123G1@users.noreply.github.com>
2023-04-05 00:03:26 +0700

    update

--- ss2_Array_Loop/bai_tap/FindMinInArray.java
+++ ss2_Array_Loop/bai_tap/FindMinInArray.java
@@ -1,10 +1,28 @@
 package ss2_Array_Loop.bai_tap;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class FindMinInArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap vao do dai mang ");
+        int size = Integer.parseInt(sc.nextLine());
+        int[] array = new int[size];
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Nhap phan tu thu" + (i + 1) + ":");
+            array[i] = Integer.parseInt(sc.nextLine());
+        }
+        System.out.println(Arrays.toString(array));
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
+            }
+        }
+        System.out.println("gia tri nho nhat cua mang la: " + min);
+
 
     }
 }
